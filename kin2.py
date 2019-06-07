edfd8994eebe0d38ecd14693ed4d6104a96e0cd1
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2019-06-07 14:21:24 +0530

    Create kin2.py

---
+++ kin2.py
@@ -0,0 +1,5 @@
+roman_str=['0','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XII','XIX','XX']
+n=str(input())
+if n in roman_str:
+    print(roman_str.index(n))
+    
